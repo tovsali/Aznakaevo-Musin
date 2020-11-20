88f489fa1d2c8f128af37123b38f2ecd55c62f9c
TovsaliPC <tovsali@mail.ru>
2020-11-20 17:07:53 +0300

    3 commit

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         x = random.randint(0, 400)
         y = random.randint(0, 300)
         x1 = random.randint(1, 100)
