78c1a3e51018148500cff9264f91139939031202
TovsaliPC <tovsali@mail.ru>
2020-11-20 16:55:29 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import random
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PIL import Image
+from PIL.ImageQt import ImageQt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog, QInputDialog
+from PyQt5.QtGui import QPixmap
+from UI import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Генератор кругов')
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(0, 400)
+        y = random.randint(0, 300)
+        x1 = random.randint(1, 100)
+        qp.drawEllipse(x, y, x1, x1)
+        print(x, y, x + x1, y + x1)
+        self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
